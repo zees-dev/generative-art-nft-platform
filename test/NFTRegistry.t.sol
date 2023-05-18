6286a8e04bea379bf915001e262439f3c8f77d2e
zees-dev <dev.786zshan@gmail.com>
2023-05-18 19:00:30 +1200

    renamed registry test file

--- test/NFTRegistry.t.sol
+++ test/NFTRegistry.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
-import "../src/NFTCollection.sol";
+import "../src/NFTRegistry.sol";
 
-contract NFTCollectionTest is Test {
-    NFTCollection public nftCollection;
+contract NFTRegistryTest is Test {
+    NFTRegistry public nftRegistry;
 
     function setUp() public {}
 
